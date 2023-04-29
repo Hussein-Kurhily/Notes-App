b95577fb04c0dd606b3ebb0e29f09b319a85b957
Hussien <kurhailyhussein@gmail.com>
2023-04-30 01:45:00 +0300

    Create the appbar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'Widgets/custom_icon_search.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+           const  SizedBox(
+              height: 28,
+            ),
+            Row(
+              children: const [
+                Text('Notes',style: TextStyle(fontSize: 28),),
+                Spacer(),
+                CustomSearchIcon()
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
