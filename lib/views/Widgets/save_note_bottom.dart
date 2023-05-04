8c12a0ed9d74043068d9e96eaf81820fe4da923b
Hussien <kurhailyhussein@gmail.com>
2023-05-05 01:20:56 +0300

    Create edite note view screen

--- lib/views/Widgets/save_note_bottom.dart
+++ lib/views/Widgets/save_note_bottom.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:notes/constant/colors.dart';
+import 'package:notes/views/Widgets/edit_note_view.dart';
 
 class SaveNoteBottom extends StatelessWidget {
   const SaveNoteBottom({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(16)
-      ),
-      child: const Center(
-        child: Text('Add', style: TextStyle(color: CupertinoColors.black, fontSize: 20),),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView() ;
+        } ));
+      } ,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(16)
+        ),
+        child: const Center(
+          child: Text('Add', style: TextStyle(color: CupertinoColors.black, fontSize: 20),),
+        ),
       ),
     );
   }
