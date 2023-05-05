cb489c745e7adeaeaea36960950747685c65d476
Hussien <kurhailyhussein@gmail.com>
2023-05-05 19:54:55 +0300

    init hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes/views/notes_view.dart';
+import 'constant/colors.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter() ;
+  await Hive.openBox(kNoteBox) ;
   runApp(const NotesApp());
 }
 class NotesApp extends StatelessWidget {
