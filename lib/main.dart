df56e6a4b91b64172d12d5318114310866fa796c
Hussien <kurhailyhussein@gmail.com>
2023-05-12 15:01:37 +0300

    Add the addNote cubit to addNote bottom shit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes/models/note_model.dart';
 import 'package:notes/views/notes_view.dart';
 import 'constant/constans.dart';
+import 'cubits/add_note_cubit.dart';
 
 void main() async {
   await Hive.initFlutter() ;
@@ -18,13 +20,18 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        home:const NotesView(),
       ),
-      home:const NotesView(),
     );
   }
 }
