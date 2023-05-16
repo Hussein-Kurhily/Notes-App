2528af7f1b2ac2159ec817e532c3a693e6fc5e59
Hussien <kurhailyhussein@gmail.com>
2023-05-16 20:53:48 +0300

    display notes part one

--- lib/cubits/notes_cubit.dart
+++ lib/cubits/notes_cubit.dart
@@ -8,16 +8,11 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-
+ List<NoteModel>? notes ;
   fetchAllNotes(NoteModel note) {
-
-    try{
       var notesBox = Hive.box<NoteModel>(kNoteBox) ;
+      notes = notesBox.values.toList() ;
 
-      emit((NotesSuccess(notesBox.values.toList())));
-    } catch (e) {
-      emit( NotesFailure(e.toString())) ;
-    }
   }
 
 }
--- lib/views/Widgets/notes_list_view.dart
+++ lib/views/Widgets/notes_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/notes_cubit.dart';
+import 'package:notes/models/note_model.dart';
 import 'custom_note_item.dart';
 
 class NoteListView extends StatelessWidget {
@@ -6,15 +9,24 @@ class NoteListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(padding: EdgeInsets.zero,
-          itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.only(bottom: 6),
-          child: NoteItem(),
-        ) ;
-      }),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: notes.length ,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.only(bottom: 6),
+                  child: NoteItem(),
+                );
+              }),
+        );
+      },
     );
   }
 }
